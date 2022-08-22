58e5f3e47f04a6b5fd60d7f30b04f66cd21eefad
valls <olavalls@gmail.com>
2022-08-21 23:00:38 -0300

    Implementando error handler

---
+++ bean-validation/src/main/java/br/com/handora/zup/handora/controllers/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,35 @@
+package br.com.handora.zup.handora.controllers.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        List<String> errorList = new ArrayList<>();
+
+        for (FieldError err : fieldErrors) {
+            String tempError = getFormat(err.getField(), err.getDefaultMessage());
+            errorList.add(tempError);
+        }
+
+        return ResponseEntity.badRequest().body(errorList);
+    }
+
+    private String getFormat(String field, String message) {
+        return String.format("Field <%s>: %s", field, message);
+    }
+}
